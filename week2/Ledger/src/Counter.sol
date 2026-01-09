ec0293a85f520472d8974f76d9c6e0092f38e1e1
vihaan1016 <vihaan1016@gmail.com>
2026-01-09 16:54:54 +0530

    Week2 day1

--- week2/Ledger/src/Counter.sol
+++ week2/Ledger/src/Counter.sol
@@ -1,14 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract Ledger {
+    // -- MAPPING--
+    mapping(address => uint256) public balance;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    // -- EVENTS--
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+
+    // -- FUNCTIONS--
+    function deposit(uint256 _amount, address _receiver) public payable {
+        require(msg.value == _amount, "Invalid amount");
+        balance[msg.sender] -= _amount;
+        balance[_receiver] += _amount;
+        emit Deposit(_receiver, _amount);
     }
 
-    function increment() public {
-        number++;
+    function withdraw(uint256 _amount) public {
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+        balance[msg.sender] -= _amount;
+        emit Withdraw(msg.sender, _amount);
+    }
+    function getYourDeposit(address user) public view returns (uint256) {
+        return balance[user];
     }
 }
