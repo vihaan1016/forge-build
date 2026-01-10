a230abec28798059361bbaee5fc363c672b14101
vihaan1016 <vihaan1016@gmail.com>
2026-01-10 20:30:18 +0530

    Feat & Fix: fixed previously broken contract and created test files

--- week2/Ledger/src/Counter.sol
+++ week2/Ledger/src/Counter.sol
@@ -10,19 +10,15 @@ contract Ledger {
     event Withdraw(address indexed user, uint256 amount);
 
     // -- FUNCTIONS--
-    function deposit(uint256 _amount, address _receiver) public payable {
-        require(msg.value == _amount, "Invalid amount");
-        balance[msg.sender] -= _amount;
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+    function transfer(uint256 _amount, address _receiver) public {
         balance[_receiver] += _amount;
-        emit Deposit(_receiver, _amount);
-    }
-
-    function withdraw(uint256 _amount) public {
-        require(balance[msg.sender] >= _amount, "Insufficient balance");
         balance[msg.sender] -= _amount;
-        emit Withdraw(msg.sender, _amount);
     }
-    function getYourDeposit(address user) public view returns (uint256) {
+    function getBalance(address user) public view returns (uint256) {
         return balance[user];
     }
 }
--- week2/Ledger/test/Counter.t.sol
+++ week2/Ledger/test/Counter.t.sol
@@ -2,23 +2,34 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Ledger} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    Ledger public ledger;
+    address public a;
+    address public b;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        ledger = new Ledger();
+        a = makeAddr("a");
+        b = makeAddr("b");
+        vm.deal(a, 100 ether);
+        vm.deal(b, 100 ether);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_Transfer() public {
+        vm.startPrank(a);
+        ledger.deposit{value: 20 ether}();
+        ledger.transfer(10 ether, b);
+        assertEq(ledger.getBalance(b), 10 ether);
+        vm.stopPrank();
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_Transfer_InsufficientBalance() public {
+        vm.startPrank(a);
+        ledger.deposit{value: 5 ether}();
+        vm.expectRevert();
+        ledger.transfer(1000 ether, b);
+        vm.stopPrank();
     }
 }
