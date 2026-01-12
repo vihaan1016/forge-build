0cdbf1a33460eda52622d15aca471aba589f719b
vihaan1016 <vihaan1016@gmail.com>
2026-01-12 16:24:38 +0530

    Feat: Increased contract functionality & testing

--- week2/Ledger/script/Counter.s.sol
+++ week2/Ledger/script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Ledger} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    Ledger public ledger;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        ledger = new Ledger();
 
         vm.stopBroadcast();
     }
--- week2/Ledger/src/Counter.sol
+++ week2/Ledger/src/Counter.sol
@@ -4,21 +4,57 @@ pragma solidity ^0.8.13;
 contract Ledger {
     // -- MAPPING--
     mapping(address => uint256) public balance;
+    mapping(address => bool) public hasReceivedInitial;
 
     // -- EVENTS--
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
+    event InitialBalanceGiven(address indexed user, uint256 amount);
+
+    // -- CONSTANTS--
+    uint256 public constant INITIAL_BALANCE = 1000 ether;
+
+    // --MODIFIERS--
+    // Modifier to automatically give initial balance
+    // Used to avoid repeated code in deposit and transfer functions
+    modifier giveInitialBalance() {
+        if (!hasReceivedInitial[msg.sender]) {
+            balance[msg.sender] = INITIAL_BALANCE;
+            hasReceivedInitial[msg.sender] = true;
+            emit InitialBalanceGiven(msg.sender, INITIAL_BALANCE);
+        }
+        _;
+    }
 
     // -- FUNCTIONS--
-    function deposit() public payable {
+    function deposit() public payable giveInitialBalance {
         balance[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
-    function transfer(uint256 _amount, address _receiver) public {
-        balance[_receiver] += _amount;
+    
+    function transfer(uint256 _amount, address _receiver) public giveInitialBalance {
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
         balance[msg.sender] -= _amount;
+        
+        // Give initial balance to receiver if they haven't received it
+        if (!hasReceivedInitial[_receiver]) {
+            balance[_receiver] = INITIAL_BALANCE;
+            hasReceivedInitial[_receiver] = true;
+            emit InitialBalanceGiven(_receiver, INITIAL_BALANCE);
+        }
+        
+        balance[_receiver] += _amount;
     }
+    
     function getBalance(address user) public view returns (uint256) {
         return balance[user];
+    }
+    
+    // Function to claim initial balance manually
+    function claimInitialBalance() public {
+        require(!hasReceivedInitial[msg.sender], "Already claimed");
+        balance[msg.sender] = INITIAL_BALANCE;
+        hasReceivedInitial[msg.sender] = true;
+        emit InitialBalanceGiven(msg.sender, INITIAL_BALANCE);
     }
 }
